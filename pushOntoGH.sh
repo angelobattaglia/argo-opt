c01c95c46f05bd02e4d88c5f4a82c5baed5aefd1
Angelo Battaglia <angbat@protonmail.com>
2021-05-11 01:48:10 +0200

    hello mom

--- pushOntoGH.sh
+++ pushOntoGH.sh
@@ -4,6 +4,6 @@ echo Enter the commit message, and will be pushed onto GitHub
 
 read message
 
-echo The commit message will be: $message
+echo The commit message will be: "$message"
 
-git add . && git commit -a -m $message && git push origin master
+git add . && git commit -a -m "$message" && git push origin master
