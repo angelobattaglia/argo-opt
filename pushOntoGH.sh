0535a085f445ab20ea74ef6e293ba025cf650f0c
Angelo Battaglia <angbat@protonmail.com>
2021-05-11 01:57:39 +0200

    hello bud

--- pushOntoGH.sh
+++ pushOntoGH.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-echo Enter the commit message, and will be pushed onto GitHub
+echo Enter the commit message and the name of the origin, and will be pushed onto GitHub
 
 read message
 
+read origin
+
+$b =
+
+if [ "$origin" = "$b" ]; then
+        "$origin" = origin
+fi
+
 echo The commit message will be: "$message"
 
-git add . && git commit -a -m "$message" && git push origin master
+git add . && git commit -a -m "$message" && git push "$origin" master
