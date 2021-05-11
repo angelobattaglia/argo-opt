38611efac87811b51cce355fce6d6e87c6cae58f
Angelo Battaglia <angbat@protonmail.com>
2021-05-11 02:03:19 +0200

    asd ads

--- pushOntoGH.sh
+++ pushOntoGH.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# make sure that your /bin/sh symlinks to a POSIX compliant shell
+
 echo Enter the commit message and the name of the origin, and will be pushed onto GitHub
 
 read message
